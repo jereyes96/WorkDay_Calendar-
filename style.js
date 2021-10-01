e54a006b343ac58c221389d10e922e8991fda222
Josue Reyes <90571004+jereyes96@users.noreply.github.com>
2021-10-01 11:02:41 -0500

    updated files
    html, css and js have been added to the file. i had issues getting the blocks to change color. not sure why my code did not work correctly

---
+++ style.js
@@ -0,0 +1,64 @@
+var date = moment().format( 'dddd MMM Do, YYYY' );
+$('#currentDay').text(date);
+
+var storedEvent = JSON.parse(localStorage.getItem('storedEvent')) || [];
+var indexTime;
+var eventBlock;
+var time;
+
+function init() {
+
+    timeColor();
+
+    for ( var i=9; i<=17; i++) {
+
+        indexTime = '#' + [i];
+    
+        $(indexTime).on('click', function(event) {
+          if (event.target.className == 'material-icons unsaved') {
+            event.target.className = 'material-icons saved';
+            eventBlock =  $(event.target).parent().siblings().children().val();
+            time = $(event.target).parent().parent().attr('id');
+            localStorage.setItem(time, eventBlock);
+          } else if (event.target.className == 'material-icons saved') {
+            event.target.className = 'material-icons unsaved';
+            eventBlock =  $(event.target).parent().siblings().children().val();
+            time = $(event.target).parent().parent().attr('id');
+            localStorage.removeItem(time);
+          };
+        });
+
+        $(indexTime).children().siblings().eq(2).children('.description').val(localStorage.getItem(i));
+        
+        if (localStorage.getItem(i) != null) {
+
+            $(indexTime).children().siblings().eq(3).children(i).removeClass('material-icons unsaved');
+            $(indexTime).children().siblings().eq(3).children(i).addClass('material-icons saved');
+            console.log($(indexTime).children().siblings().eq(3).children(i));
+        };
+    };
+
+};
+
+function timeColor() {
+    for (var i=9; i<=17; i++) {
+        indexTime = '#' + [i];
+
+        if( i < moment().format('H') ){
+            $(indexTime).addClass('past');
+            $(indexTime).removeClass('future');
+            $(indexTime).removeClass('present');
+        } else if (i > moment().format('H') ) {
+            $(indexTime).addClass('funture');
+            $(indexTime).removeClass('past');
+            $(indexTime).removeClass('present');
+        } else if ( i=== moment().format('H') ) {
+            $(indexTime).addClass('present');
+            $(indexTime).removeClass('future');
+            $(indexTime).removeClass('past');
+        };
+    };
+};
+
+
+init();
